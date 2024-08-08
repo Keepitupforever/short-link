511c93d38e41de3583eef606c81e201a8c53fb30
liuyelei <1521024693@qq.com>
2024-08-08 14:43:16 +0800

    第二次提交

---
+++ admin/src/main/java/com/liuyelei/shortlink/admin/ShortLinkApplication.java
@@ -0,0 +1,14 @@
+package com.liuyelei.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.liuyelei.shortlink.admin.dao.mapper")
+public class ShortLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkApplication.class, args);
+    }
+}
---
+++ admin/src/main/java/com/liuyelei/shortlink/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.liuyelei.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liuyelei.shortlink.admin.dao.entity.UserDO;
+
+/**
+ * 用户持久层
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}
