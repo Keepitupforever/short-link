58f066e3fc2eb02c9463d9c51d5e70409cfe9884
liuyelei <1521024693@qq.com>
2024-08-10 14:56:39 +0800

    feature: 开发短链接后管分组新增功能

--- admin/src/main/java/com/liuyelei/shortlink/admin/dao/entity/GroupDO.java
+++ admin/src/main/java/com/liuyelei/shortlink/admin/dao/entity/GroupDO.java
@@ -1,18 +1,21 @@
 package com.liuyelei.shortlink.admin.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.liuyelei.shortlink.admin.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-
-import java.util.Date;
+import lombok.NoArgsConstructor;
 
 /**
  * 短链接分组实体
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @TableName("t_group")
-public class GroupDO {
+public class GroupDO extends BaseDO {
 
 
     /**
@@ -39,21 +42,4 @@ public class GroupDO {
      * 分组排序
      */
     private Integer sortOrder;
-
-    /**
-     * 创建时间
-     */
-    private Date createTime;
-
-    /**
-     * 修改时间
-     */
-    private Date updateTime;
-
-    /**
-     * 删除标识 0：未删除 1：已删除
-     */
-    private Integer delFlag;
-
-    public GroupDO() {}
 }
--- admin/src/main/java/com/liuyelei/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/liuyelei/shortlink/admin/service/GroupService.java
@@ -7,4 +7,10 @@ import com.liuyelei.shortlink.admin.dao.entity.GroupDO;
  * 短链接分组接口
  */
 public interface GroupService extends IService<GroupDO> {
+    /**
+     * 新增短链接分组
+     *
+     * @param groupName 短链接分组名
+     */
+    void saveGroup(String groupName);
 }
