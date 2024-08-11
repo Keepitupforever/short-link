14f2ce246693239e25c469b3257e08b9cdcd41e6
liuyelei <1521024693@qq.com>
2024-08-11 22:11:06 +0800

    feature:

--- admin/src/main/java/com/liuyelei/shortlink/admin/controller/ShortLinkController.java
+++ admin/src/main/java/com/liuyelei/shortlink/admin/controller/ShortLinkController.java
@@ -9,10 +9,7 @@ import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
 import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
 import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 短链接后管控制层
