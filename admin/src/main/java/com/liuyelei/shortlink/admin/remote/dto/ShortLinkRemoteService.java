294d801ce37ca9c43662f59594ee45bf3d282862
liuyelei <1521024693@qq.com>
2024-08-11 16:12:26 +0800

    feature: 短链接后管系统调用中台请求相关接口

---
+++ admin/src/main/java/com/liuyelei/shortlink/admin/remote/dto/ShortLinkRemoteService.java
@@ -0,0 +1,45 @@
+package com.liuyelei.shortlink.admin.remote.dto;
+
+import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.TypeReference;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.liuyelei.shortlink.admin.common.convention.result.Result;
+import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface ShortLinkRemoteService {
+
+    /**
+     * 创建短链接
+     * @param requestParam 创建短链接请求参数
+     * @return 创建短链接响应
+     */
+    default Result<ShortLinkCreateRespDTO> createShortLink(ShortLinkCreateReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/create", JSON.toJSONString(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<Result<ShortLinkCreateRespDTO>>() {
+        });
+    }
+
+    /**
+     * 短链接分页查询
+     * @param requestParam 短链接分页查询请求参数
+     * @return 短链接分页查询响应
+     */
+    default Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("gid", requestParam.getGid());
+        requestMap.put("current", requestParam.getCurrent());
+        requestMap.put("size", requestParam.getSize());
+        String resultPage = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/page", requestMap);
+        return JSON.parseObject(resultPage, new TypeReference<Result<IPage<ShortLinkPageRespDTO>>>() {
+        });
+    }
+
+
+}
