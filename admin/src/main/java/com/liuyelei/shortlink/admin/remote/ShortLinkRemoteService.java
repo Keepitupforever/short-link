0d5375763df2d1b41f9785859520584dbea806b6
liuyelei <1521024693@qq.com>
2024-08-14 16:31:34 +0800

    feature: 开发单个短链接监控查询和短链接后管远程调用单个短链接监控查询功能

--- admin/src/main/java/com/liuyelei/shortlink/admin/remote/ShortLinkRemoteService.java
+++ admin/src/main/java/com/liuyelei/shortlink/admin/remote/ShortLinkRemoteService.java
@@ -1,5 +1,6 @@
 package com.liuyelei.shortlink.admin.remote;
 
+import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.TypeReference;
@@ -8,13 +9,11 @@ import com.liuyelei.shortlink.admin.common.convention.result.Result;
 import com.liuyelei.shortlink.admin.dto.req.RecycleBinRecoverReqDTO;
 import com.liuyelei.shortlink.admin.dto.req.RecycleBinRemoveReqDTO;
 import com.liuyelei.shortlink.admin.dto.req.RecycleBinSaveReqDTO;
-import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
-import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
-import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
-import com.liuyelei.shortlink.admin.remote.dto.req.ShortLinkUpdateReqDTO;
+import com.liuyelei.shortlink.admin.remote.dto.req.*;
 import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
 import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkGroupCountQueryRespDTO;
 import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import com.liuyelei.shortlink.admin.remote.dto.resp.ShortLinkStatsRespDTO;
 
 import java.util.HashMap;
 import java.util.List;
@@ -121,5 +120,18 @@ public interface ShortLinkRemoteService {
      */
     default void removeRecycleBin(RecycleBinRemoveReqDTO requestParam) {
         HttpUtil.post("http://127.0.0.1:8001/api/short-link/v1/recycle-bin/remove", JSON.toJSONString(requestParam));
+    }
+
+
+    /**
+     * 访问单个短链接指定时间内监控数据
+     *
+     * @param requestParam 访问短链接监控请求参数
+     * @return 短链接监控信息
+     */
+    default Result<ShortLinkStatsRespDTO> oneShortLinkStats(ShortLinkStatsReqDTO requestParam) {
+        String resultBodyStr = HttpUtil.get("http://127.0.0.1:8001/api/short-link/v1/stats", BeanUtil.beanToMap(requestParam));
+        return JSON.parseObject(resultBodyStr, new TypeReference<>() {
+        });
     }
 }
