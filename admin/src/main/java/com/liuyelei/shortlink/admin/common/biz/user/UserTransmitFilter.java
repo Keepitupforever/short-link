12a6ef59d297ca97d3b11e0f0d51201b77fac860
liuyelei <1521024693@qq.com>
2024-08-18 15:07:29 +0800

    optimize: 优化短链接相关缓存key规范

--- admin/src/main/java/com/liuyelei/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/com/liuyelei/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -16,6 +16,7 @@ import java.io.PrintWriter;
 import java.util.List;
 import java.util.Objects;
 
+import static com.liuyelei.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
 import static com.liuyelei.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
@@ -47,7 +48,7 @@ public class UserTransmitFilter implements Filter {
                 }
                 Object userInfoJsonStr = null;
                 try {
-                    userInfoJsonStr = stringRedisTemplate.opsForHash().get("login_" + username, token);
+                    userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
                     if (userInfoJsonStr == null) {
                         throw new ClientException(USER_TOKEN_FAIL);
                     }
