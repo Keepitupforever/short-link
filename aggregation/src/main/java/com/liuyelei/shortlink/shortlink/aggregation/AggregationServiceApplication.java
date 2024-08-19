16ed8c16d6c25986819b7e91f40456043bb09e7e
liuyelei <1521024693@qq.com>
2024-08-19 16:50:22 +0800

    feature: 开发短链接系统聚合服务

---
+++ aggregation/src/main/java/com/liuyelei/shortlink/shortlink/aggregation/AggregationServiceApplication.java
@@ -0,0 +1,26 @@
+package com.liuyelei.shortlink.shortlink.aggregation;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 短链接聚合应用
+ */
+@SpringBootApplication(scanBasePackages = {
+        "com.liuyelei.shortlink.admin",
+        "com.liuyelei.shortlink.project",
+        "com.liuyelei.shortlink.aggregation"
+})
+@EnableDiscoveryClient
+@MapperScan(value = {
+        "com.liuyelei.shortlink.project.dao.mapper",
+        "com.liuyelei.shortlink.admin.dao.mapper"
+})
+public class AggregationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregationServiceApplication.class, args);
+    }
+}
